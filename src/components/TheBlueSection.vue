7d2e384f39a4afe5f38c5be69a97a4b681243e47
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-21 16:31:13 +0200

    blue section partialy v-for but completed

--- src/components/TheBlueSection.vue
+++ src/components/TheBlueSection.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
         <div class="container-fluid myBlueBg">
-            <div class="container d-flex">
+            <div class="container d-flex justify-content-between align-items-center py-5">
                 <div v-for="card in cardsLinks" :key="card">
-                    <img :src="card.img" alt="">
-                    <span>{{card.link}}</span>
+                    <img class="cardImgOne" :src="card.img" alt="">
+                    <span class="text-white">{{card.link}}</span>
+                </div>
+                <div>
+                    <img class="cardImgTwo" src="/img/buy-comics-shop-locator.png" alt="">
+                    <span class="text-white">COMIC SHOP LOCATOR</span>
+                </div>
+                <div>
+                    <img class="cardImgThree" src="/img/buy-dc-power-visa.png" alt="">
+                    <span class="text-white">DC POWER VISA</span>
                 </div>
             </div>
         </div>
@@ -22,21 +30,14 @@ export default{
                     link: "DIGITAL COMICS",
                 },
                 {
-                    img: "/img/buy-comics-digital-comics.png",
-                    link: "DIGITAL COMICS",
+                    img: "/img/buy-comics-merchandise.png",
+                    link: "DC MERCHANDISE",
                 },
                 {
-                    img: "/img/buy-comics-digital-comics.png",
-                    link: "DIGITAL COMICS",
+                    img: "/img/buy-comics-subscriptions.png",
+                    link: "SUBSCRIPTION",
                 },
-                {
-                    img: "/img/buy-comics-digital-comics.png",
-                    link: "DIGITAL COMICS",
-                },
-                {
-                    img: "/img/buy-comics-digital-comics.png",
-                    link: "DIGITAL COMICS",
-                },
+                
             ]
         }
     }
@@ -46,5 +47,14 @@ export default{
 <style>
     .myBlueBg{
         background-color: #0282f9;
+    }
+    .cardImgOne{
+        width: 50px;
+    }
+    .cardImgTwo{
+        width: 35px;
+    }
+    .cardImgThree{
+        width: 55px;
     }
 </style>
