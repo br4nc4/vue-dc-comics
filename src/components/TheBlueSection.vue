2f215b31cc52fc5ffe1952c2bcca2b4cc0f83989
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-21 16:05:27 +0200

    blue section v-for structure

--- src/components/TheBlueSection.vue
+++ src/components/TheBlueSection.vue
@@ -2,15 +2,46 @@
     <div>
         <div class="container-fluid myBlueBg">
             <div class="container d-flex">
-                <div>
-                    <img src="/img/buy-comics-digital-comics.png" alt="">
+                <div v-for="card in cardsLinks" :key="card">
+                    <img :src="card.img" alt="">
+                    <span>{{card.link}}</span>
                 </div>
             </div>
         </div>
     </div>
 </template>
 
-
+<script>
+export default{
+    data(){
+        return{
+            cardsLinks:
+            [
+                {
+                    img: "/img/buy-comics-digital-comics.png",
+                    link: "DIGITAL COMICS",
+                },
+                {
+                    img: "/img/buy-comics-digital-comics.png",
+                    link: "DIGITAL COMICS",
+                },
+                {
+                    img: "/img/buy-comics-digital-comics.png",
+                    link: "DIGITAL COMICS",
+                },
+                {
+                    img: "/img/buy-comics-digital-comics.png",
+                    link: "DIGITAL COMICS",
+                },
+                {
+                    img: "/img/buy-comics-digital-comics.png",
+                    link: "DIGITAL COMICS",
+                },
+            ]
+        }
+    }
+}
+</script>
 
 <style>
     .myBlueBg{
