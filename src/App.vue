98d773579b42370e45bb6729ad250e05cd455ef7
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-21 19:17:58 +0200

    footer pt.1

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <TheBlackSection/>
     <TheBlueSection/>
     <TheBannerFooter/>
+    <TheFooter/>
   </div>
 </template>
 
@@ -12,13 +13,15 @@ import TheHeader from './components/TheHeader.vue'
 import TheBlackSection from './components/TheBlackSection.vue'
 import TheBlueSection from './components/TheBlueSection.vue'
 import TheBannerFooter from './components/TheBannerFooter.vue'
+import TheFooter from './components/TheFooter.vue'
 
 export default {
   components: {
     TheHeader,
     TheBlackSection,
     TheBlueSection,
-    TheBannerFooter
+    TheBannerFooter,
+    TheFooter
 }
 }
 </script>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div class="conatiner-fluid bgBottomFooter">
+            <div class="container d-flex justify-content-between">
+                <div>
+                    <button type="button" class="myButton">SIGN-UP NOW!</button>
+                </div>
+
+                <div class="d-flex">
+                    <div>
+                        <h5 class="followColor">FOLLOW US</h5>
+                    </div>
+
+                    <div>
+                        <img src="/img/footer-facebook.png" alt="">
+                    </div>
+                    <div>
+                        <img src="/img/footer-twitter.png" alt="">
+                    </div>
+                    <div>
+                        <img src="/img/footer-youtube.png" alt="">
+                    </div>
+                    <div>
+                        <img src="/img/footer-pinterest.png" alt="">
+                    </div>
+                    <div>
+                        <img src="/img/footer-periscope.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    .bgBottomFooter{
+        background-color:#303030;
+    }
+
+    .myButton{
+        background-color: #303030;
+        border: 2px solid #0282f9;
+        color: white;
+        padding: 0.5rem 0.5rem;
+    }
+
+    .followColor{
+        color: #1b69d9;
+    }
+</style>
