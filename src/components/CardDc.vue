80ad9002852485aef939c124566ca09a110e6560
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-22 17:28:07 +0200

    Comic cards added after many errors

---
+++ src/components/CardDc.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="card">
+            <img class="card-img-top" :src="image" alt="">
+            <div class="card-body">{{series}}</div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "CardDc",
+    props: {
+        series: String,
+        image: String,
+        type: String,
+        price: String,
+    },
+};
+</script>
+
+<style>
+
+</style>
